258447dc945a99920044345a03c82bac76d2feb6
Theardy <Theardy10@gmail.com>
2018-09-05 19:11:59 +0300

    firt commit corejava

---
+++ linkedList/LinkedListTest.java
@@ -0,0 +1,64 @@
+
+import java.util.*;
+
+/**
+ * This program demonstrates operations on linked lists.
+ * @version 1.11 2012-01-26
+ * @author Cay Horstmann
+ */
+public class LinkedListTest
+{
+   public static void main(String[] args)
+   {
+      List<String> a = new LinkedList<>();
+      a.add("Amy");
+      a.add("Carl");
+      a.add("Erica");
+
+      List<String> b = new LinkedList<>();
+      b.add("Bob");
+      b.add("Doug");
+      b.add("Frances");
+      b.add("Gloria");
+      b.add(1, "Suka"); 
+      System.out.println(b);
+      b.indexOf("Suka");
+      System.out.println("begin");
+      int c = b.indexOf("Fra");
+      System.out.println("end "+c);
+      // merge the words from b into a
+
+      ListIterator<String> aIter = a.listIterator();
+      Iterator<String> bIter = b.iterator();
+
+      while (bIter.hasNext())
+      {
+         if (aIter.hasNext()) aIter.next();
+         aIter.add(bIter.next());
+      }
+
+      System.out.println(a);
+
+      // remove every second word from b
+
+      bIter = b.iterator();
+      while (bIter.hasNext())
+      {
+         bIter.next(); // skip one element
+         if (bIter.hasNext())
+         {
+            bIter.next(); // skip next element
+            bIter.remove(); // remove that element
+         }
+      }
+      b.indexOf(b);
+      System.out.println(b);
+
+      // bulk operation: remove all words in b from a
+
+      a.removeAll(b);
+
+      System.out.println(a);
+      System.out.println(a);
+   }
+}
